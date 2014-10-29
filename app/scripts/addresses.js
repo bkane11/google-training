d6b7e059d60ceae2cb8a01122495d4984e3b43c7
Ben Kane <benkane11@gmail.com>
2014-10-29 08:11:44 -0700

    fixed

--- app/scripts/addresses.js
+++ app/scripts/addresses.js
@@ -1,88 +1,174 @@
 var addresses = {
-	features: [
-		{
-			name: 'Rincon Beach Park Paninis',
-			address: '610 Mission Canyon Rd, Santa Barbara, CA 93105'
-		},
-		{
-			name: 'Pillar Point Patio',
-			address: '300 W Point Ave,Half Moon Bay, CA 94019'
-		},
-		{
-			name: 'Santa Cruz Surfin Museum and Food Stop',
-			address: '701 W Cliff Dr, Santa Cruz, CA 95060701'
-		},
-		{
-			name: 'Surfers Point Park Picnic Place',
-			address: 'Shoreline Dr, Ventura, CA 93001'
-		},
-		{
-			name: 'Malibu Lagoon State Beach BBQ',
-			address: '23200 Pacific Coast Hwy, Malibu, CA 90265'
-		},
-		{
-			name: 'Long Beach Lodge Resort and Restaurant',
-			address: '1441 Pacific Rim Hwy, Tofino, BC V0R 2Z0, Canada'
-		},
-		{
-			name: 'San Onofre State Beach Buffet',
-			address: 'Old Highway 101, San Clemente, CA 92672'
-		},
-		{
-			name: "Elizabeth's Market",
-			address: '23040 E Cliff Dr, Santa Cruz, CA 95062'
-		},
-		{
-			name: 'Año Nuevo State Park Fast',
-			address: '1 New Years Creek Rd, Pescadero, CA 94060'
-		},
-		{
-			name: 'Phil\'s Fish Market & Eatery',
-			address: '7600 Sandholdt Rd, Moss Landing, CA 95039'
-		},
-		{
-			name: 'Dolphin Restaurant',
-			address: '71 Municipal Wharf, Santa Cruz, CA 95060'
-		},
-		{
-			name: 'Davenport Roadhouse Restaurant and Inn',
-			address: '1 Davenport Ave, Davenport, CA 95017'
-		},
-		{
-			name: 'Swanton Berry Farm',
-			address: '25 Swanton Rd, Davenport, CA 95017'
-		},
-		{
-			name: 'Point Reyes Lighthouse Visitor Information Center and Chophouse',
-			address: '27000 Sir Francis Drake Blvd, Inverness, CA 94937'
-		},
-		{
-			name: 'Pier Chowder House & Tap Room',
-			address: '790 Port Rd, Point Arena, CA 95468'
-		},
-		{
-			name: 'Griffin House Inn',
-			address: '5910 California 1, Elk, CA 95432'
-		},
-		{
-			name: 'Chart Room Restaurant',
-			address: '130 Anchor Way, Crescent City, CA 95531'
-		},
-		{
-			name: 'Spinner\'s Seafood Steak & Chop House',
-			address: '29430 Ellensburg Ave, Gold Beach, OR 97444'
-		},
-		{
-			name: 'The Crazy Norwegians Fish and Chips',
-			address: '259 6th St, Port Orford, OR 97465'
-		},
-		{
-			name: 'Griff\'s On the Dock',
-			address: '210 Dock Rd, Port Orford, OR 97465'
-		},
-		{
-			name: 'Salty Dawg Saloon',
-			address: '4380 Homer Spit Rd, Homer, AK 99603'
-		}
-]}
-
+  "features": [
+    {
+      "name": "Rincon Beach Park Paninis",
+      "address": "610 Mission Canyon Rd, Santa Barbara, CA 93105",
+      "latlng": [
+        34.4408592,
+        -119.71285929999999
+      ]
+    },
+    {
+      "name": "Pillar Point Patio",
+      "address": "West Point Avenue, Half Moon Bay, CA 94019",
+      // "address": "300 West Point Avenue, Half Moon Bay, CA 94019",
+      // "address": "300 W Point Ave, Half Moon Bay, CA 94019",
+      "latlng": [
+        37.503373,
+        -122.49472230000003
+      ]
+    },
+    {
+      "name": "Santa Cruz Surfin Museum and Food Stop",
+      "address": "701 West Cliff Dr, Santa Cruz, CA 95060",
+      "latlng": [
+        36.951449,
+        -122.02667989999998
+      ]
+    },
+    {
+      "name": "Surfers Point Park Picnic Place",
+      "address": "Shoreline Dr, Ventura, CA 93001",
+      "latlng": [
+        34.2745307,
+        -119.3005384
+      ]
+    },
+    {
+      "name": "Malibu Lagoon State Beach BBQ",
+      "address": "23200 Pacific Coast Hwy, Malibu, CA 90265",
+      "latlng": [
+        34.0344382,
+        -118.67952179999997
+      ]
+    },
+    {
+      "name": "Long Beach Lodge Resort and Restaurant",
+      "address": "1441 Pacific Rim Highway, Tofino, BC V0R",
+      "latlng": [
+        49.1066571,
+        -125.86797919999998
+      ]
+    },
+    {
+      "name": "San Onofre State Beach Buffet",
+      "address": "Old Highway 101, San Clemente, CA 92672",
+      "latlng": [
+        33.3812955,
+        -117.57369030000001
+      ]
+    },
+    {
+      "name": "Elizabeth's Market",
+      "address": "23040 E Cliff Dr, Santa Cruz, CA 95062",
+      "latlng": [
+        36.955626,
+        -121.97243700000001
+      ]
+    },
+    {
+      "name": "Año Nuevo State Park Fast",
+      "address": "New Years Creek Rd, Pescadero, CA 94060",
+      "latlng": [
+        37.1196843,
+        -122.30679900000001
+      ]
+    },
+    {
+      "name": "Phil's Fish Market & Eatery",
+      "address": "7600 Sandholdt Rd, Moss Landing, CA 95039",
+      "latlng": [
+        36.803346,
+        -121.78773100000001
+      ]
+    },
+    {
+      "name": "Dolphin Restaurant",
+      "address": "71 Municipal Wharf, Santa Cruz, CA 95060",
+      "latlng": [
+        36.957558,
+        -122.01737400000002
+      ]
+    },
+    {
+      "name": "Davenport Roadhouse Restaurant and Inn",
+      "address": "1 Davenport Ave, Davenport, CA 95017",
+      "latlng": [
+        37.0108619,
+        -122.19388200000003
+      ]
+    },
+    {
+      "name": "Swanton Berry Farm",
+      "address": "25 Swanton Rd, Davenport, CA 95017",
+      "latlng": [
+        37.0303339,
+        -122.21847530000002
+      ]
+    },
+    {
+      "name": "Point Reyes Lighthouse Visitor Information Center and Chophouse",
+      "address": "27000 Sir Francis Drake Blvd, Inverness, CA 94937",
+      "latlng": [
+        37.9965067,
+        -123.02050759999997
+      ]
+    },
+    {
+      "name": "Pier Chowder House & Tap Room",
+      "address": "790 Port Rd, Point Arena, CA 95468",
+      "latlng": [
+        38.9146233,
+        -123.70941879999998
+      ]
+    },
+    {
+      "name": "Griffin House Inn",
+      "address": "5910 California 1, Elk, CA 95432",
+      "latlng": [
+        39.1338873,
+        -123.71847680000002
+      ]
+    },
+    {
+      "name": "Chart Room Restaurant",
+      "address": "130 Anchor Way, Crescent City, CA 95531",
+      "latlng": [
+        41.7438102,
+        -124.1810585
+      ]
+    },
+    {
+      "name": "Spinner's Seafood Steak & Chop House",
+      "address": "29430 Ellensburg Ave, Gold Beach, OR 97444",
+      "latlng": [
+        42.4064127,
+        -124.4219104
+      ]
+    },
+    {
+      "name": "The Crazy Norwegians Fish and Chips",
+      "address": "259 6th St, Port Orford, OR 97465",
+      "latlng": [
+        42.7435023,
+        -124.49451479999999
+      ]
+    },
+    {
+      "name": "Griff's On the Dock",
+      "address": "Dock Road, Port Orford, OR 97465",
+      "latlng": [
+        42.7417009,
+        -124.49864760000003
+      ]
+    },
+    {
+      "name": "Salty Dawg Saloon",
+      "address": "4380 Homer Spit Rd, Homer, AK 99603",
+      "latlng": [
+        59.6063736,
+        -151.43386269999996
+      ]
+    }
+  ]
+}
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,8 @@
-var map, geocoder, autocomplete, distanceMatrix
+var map, geocoder, autocomplete, distanceMatrix, directionsDisplay, directionsService,
 	storesReady,
-	stores = []
+	directionsLayers = [],
+	stores = [],
+	closestStores,
 	closestNumber = 5;
 
 function initialize() {
@@ -12,43 +14,93 @@ function initialize() {
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   locate('init');
   geocodeStores();
+  // addStoresByLatLng();
 
   autocomplete = new google.maps.places.Autocomplete($('input.location')[0]);
   autocomplete.bindTo('bounds', map);
   google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
   distanceMatrix = new google.maps.DistanceMatrixService();
+  directionsService = new google.maps.DirectionsService();
 }
 
 function getDrivingDistance(start, end, cb){
+	showLoading();
+	hideLayers(directionsLayers);
+	directionsLayers = [];
+	end = stores.map(function(a){return $(a.info)[1].innerText});
 	distanceMatrix.getDistanceMatrix(
-  {
-    origins: [start],
-    destinations: [end],
-    travelMode: google.maps.TravelMode.DRIVING,
-    unitSystem: google.maps.UnitSystem.IMPERIAL,
-    durationInTraffic: false,
-    avoidHighways: false,
-    avoidTolls: false
-  }, cb);
+	  {
+	    origins: [start],
+	    destinations: end instanceof Array ? end : [end],
+	    travelMode: google.maps.TravelMode.DRIVING,
+	    unitSystem: google.maps.UnitSystem.IMPERIAL,
+	    durationInTraffic: false,
+	    avoidHighways: false,
+	    avoidTolls: false
+	  }, cb
+	 );
 }
 
-function showAllStores(){
-	if(!storesReady)
+function handleDistanceMatrixResult(response, status){
+	if (status != google.maps.DistanceMatrixStatus.OK) {
+    alert('Error was: ' + status);
+  } else {
+    var origins = response.originAddresses;
+    var destinations = response.destinationAddresses
+
+    for (var i = 0; i < origins.length; i++) {
+      var results = response.rows[i].elements;
+      var closest = results.slice(0) // clone first
+      	.sort(function(a,b){
+	      	  if (a.distance.value < b.distance.value) {
+			    return -1;
+			  }
+			  if (a.distance.value > b.distance.value) {
+			    return 1;
+			  }
+			  return 0;
+	      })
+      	.slice(0, closestNumber) // return only the closest;
+   	  var addresses = closest.map(function(a){
+   	  	// calcRoute(origins[0], destinations[results.indexOf(a)]);
+   	  	return [destinations[results.indexOf(a)], a]
+   	  })
+
+   	  getClosest(addresses, origins[0]);
+    }
+  }
+}
+
+
+function showLayers(group){
+	if((!group || (group && group===stores) ) && !storesReady)
 		return alert('Oops, stores are still loading.  Google\'s geocoding api did not allow the results to load this fast');
-	stores.forEach(function(store){
-		console.log(store);
-		store.setVisible(true);
+	group = group || stores;
+	
+	if(!group instanceof Array || !group.length)
+		group = [group]
+
+	group.forEach(function(layer){
+		layer.setMap(map);
+		// layer.setVisible(true);
 	})
+	$('.showStores').removeClass('show');
+	return group
 }
 
-function hideAllStores(){
-	stores.forEach(function(store){
-		store.setVisible(false);
+function hideLayers(group){
+	group = group || stores
+	group.forEach(function(layer){
+		layer.setMap(null);
 	})
+	$('.showStores').addClass('show');
+	return group
 }
 
-function updateLoadingInfo(html){
-	$('.loadingInfo').html(html)
+function addStoresByLatLng(){
+	addresses.features.forEach(function(store){
+		addMarker(new google.maps.LatLng(store.latlng[0], store.latlng[1]), ['<h5>'+(store.name || 'unknown')+ '</h5>', '<span>' + (store.address|| store) + '</span>'].join(''), stores);
+	})
 }
 
 function geocodeStores(n){
@@ -56,49 +108,94 @@ function geocodeStores(n){
 	if(n>addresses.features.length) return
 	var store = addresses.features[n];
 	if(!store) return
-	updateLoadingInfo('loading store ' + n + ' of ' + addresses.features.length)
-	codeAddress(store, addMarker, n);
+	showLoading('loading... store ' + n + ' of ' + addresses.features.length);
+	codeAddress(store, addMarker, n, stores);
 }
 
-function codeAddress(store, cb, n) {
-  geocoder.geocode( { 'address': store.address}, function(results, status) {
+function codeAddress(store, cb, n, stores) {
+  geocoder.geocode( { 'address': store.address || store}, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
-    	cb(results[0].geometry.location, ['<h5>'+store.name+ '</h5>', store.address].join('') );
-    	setTimeout( function(){
-    		geocodeStores(n+1);
-    	 }, 300);
+    	cb(results[0].geometry.location, ['<h5>'+(store.name || 'unknown')+ '</h5>', '<span>' + (store.address|| store) + '</span>'].join(''), stores );    	
+    	if(n>=0)
+	    	setTimeout( function(){
+	    		geocodeStores(n+1, stores);
+	    	 }, 300);
     } else {
       console.log('Geocode was not successful for the following reason: ' + status);
-      setTimeout( function(){
-    		codeAddress(store, cb, n);
-       }, 1000);
+	      setTimeout( function(){
+	    		codeAddress(store, cb, n, stores);
+	       }, 1000);
     }
   });
 }
 
-function getClosest(location){
+function getClosest(locations, origin){
 	var closest = [];
-	stores.forEach(function(store){
-		if(store.position)
-			closest.push([store, google.maps.geometry.spherical.computeDistanceBetween(location, store.position)]);//, radius?:number)
-	})
+	hideLayers(stores);
+	if(locations instanceof Array || typeof locations==='string')
+		locations.forEach(function(location, l_index){
+			var distanceInfo = location[1];
+			location = location[0];
+			stores.forEach(function(store, s_index){
+				var info = $(store.info),
+					addressNumLocation = location.split(' ')[0].trim(),
+					addressNumStore = info[1].innerText.split(' ')[0].trim(),
+					storeParts = info[1].innerText.split(','),
+					locationParts = location.split(',');
+				if(addressNumStore === addressNumLocation  && locationParts[locationParts.length-2].trim() == storeParts[storeParts.length-1].trim() )
+					closest.push([store, distanceInfo]),
+					calcRoute(origin, info[1].innerText, store);
+			})
+		})
+	else
+		return getDrivingDistance(map.getCenter(), stores, handleDistanceMatrixResult)
 
 	$('ul.closest').empty();
-	return closest.sort(function(a,b){return a[1] - b[1]}).slice(0,closestNumber)
-		.forEach(function(marker){
-			marker[0].setVisible(true);
-			var info =  $(marker[0].info);
-			var item = $('<li></li>').html(marker[0].info)
-				.on('click', function(){showInfoWindow(marker[0], info.find('h5').remove() )})
-				.attr('title', info[0].innerText)
-				.appendTo('ul.closest')
+	closestStores = closest
+		.slice(0,closestNumber)
+		.forEach(function(item){
+			var marker = item[0], itemInfo = item[1];
+			if(!marker) return
+			var info =  $(marker.info);
+			var item = $('<li></li>')
+				.on('click', function(){
+					showInfoWindow(marker, marker.info );
+					hideLayers(directionsLayers);
+					var currentCenter;
+					if(marker.directions)
+						showLayers([marker, marker.directions]),
+						marker.directions.setPanel( $('#directions').empty()[0] ),
+						currentCenter = map.getCenter(),
+						$('.cansplit:not(.split)').addClass('split'),
+						map.getCenter(currentCenter),
+						showDirectionsButton() // recenter the map after panel split
+				})
+				.html( info.append('<i>' + ['',itemInfo.distance.text, itemInfo.duration.text].join('<br/>') + '</i>').html() )
+				.attr('title', info[1].innerText )
+				.appendTo('ul.closest');
+			return marker.setMap(map);
 		});
 }
 
-function setCenter(position){
+var meMarker;
+
+function markMe(pos){
+	var icon = 'https://chart.googleapis.com/chart?chst=d_map_spin&chld=0.5|0|FFFF42|10|b|ME';
+	!meMarker ? meMarker = new google.maps.Marker({
+	      position: pos,
+	      map: map,
+	      zIndex: 999,
+	      visible: true,
+	      icon: icon
+	}) : meMarker.setPosition(pos);
+}
+
+function setCenter(position, init){
 	var pos = position.coords ? new google.maps.LatLng(position.coords.latitude, position.coords.longitude) : position;
 	map.setCenter(pos);
-	getClosest(pos);
+	markMe(pos);
+	if(init!=='init')
+		getClosest(pos);
 }
 
 function showInfoWindow(marker, info){
@@ -114,45 +211,70 @@ function showInfoWindow(marker, info){
   	  	map.infowindow = new google.maps.InfoWindow(options);
 }
 
-function addMarker(latlng, info){
-	  // var image = 'images/beachflag.png';
+function addMarker(latlng, info, stores){
 	  var marker = new google.maps.Marker({
 	      position: latlng,
-	      map: map,
-	      visible: false,
 	      info: info
-	      // icon: image
 	  });
 	  google.maps.event.addListener(marker, 'click', function() {
 	    showInfoWindow(marker);
 	  });
-	  // console.log(marker);
-	  stores.push(marker);
-	  if(stores.length===addresses.features.length)
-	  		storesReady = true,
-	  		hideLoading(),
-	 		displayShowStoresButton();
+
+	  if(stores){
+		  if(stores.length<addresses.features.length)
+			  stores.push(marker);
+		  if(stores.length===addresses.features.length)
+		  		storesReady = true,
+		  		hideLoading(),
+		 		displayShowStoresButton();
+	  }else
+	  	console.log('no stores');
 }
 
 function displayShowStoresButton(){
 	$('.showStores')
+		.removeClass('hidden')
 		.off('click')
-		.on('click', showAllStores)
-		.removeClass('hidden');
+		.on('click', function(){
+			if($(this).hasClass('show'))
+				showLayers(stores)
+			else
+				hideLayers([].concat(directionsLayers,stores)) 
+		})
+}
+function showDirectionsButton(){
+	var currentCenter;
+	$('.showDirections')
+		.removeClass('hidden')
+		.off('click')
+		.on('click', function(){
+			if($(this).hasClass('show'))
+				showLayers(directionsLayers)
+			else
+				hideLayers([].concat(directionsLayers,closestStores || [] )),
+				$(this).addClass('hidden'),
+				currentCenter = map.getCenter(),
+				clearResults()
+				map.getCenter(currentCenter)
+		})
+}
+
+function clearResults(){
+	$('#directions').empty();
+	$('.split').removeClass('split');
 }
 
 function hideLoading(){
-	$('.title').removeClass('loading');
-	updateLoadingInfo('');
+	$('.loading').fadeOut();
 }
-function showLoading(){
-	$('.title').addClass('loading');
+function showLoading(html){
+	$('.loading').html(html || 'loading...').fadeIn();
 }
 
 function locate(e){
 	  if(navigator.geolocation) {
 	    navigator.geolocation.getCurrentPosition(
-	    	setCenter,
+	    	function(result){setCenter(result, e)},
 	    	function() { 
 		      handleNoGeolocation(true);
 		 });
@@ -205,8 +327,43 @@ function onPlaceChanged(e) {
       map.setZoom(14);  // Why 17? Because it looks good.
     }
 
-    getClosest(map.getCenter());
+    clearResults();
+    markMe(map.getCenter())
+    getDrivingDistance(map.getCenter(), stores, handleDistanceMatrixResult)
+    // getClosest(map.getCenter());
   }
+
+function calcRoute(start, end, marker) {
+  var request = {
+      origin:start,
+      destination:end,
+      travelMode: google.maps.TravelMode.DRIVING
+  };
+
+  var rendererOptions = {
+  	// suppressInfoWindows: true,
+  	suppressMarkers: true
+  }
+
+  var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
+  directionsDisplay.setMap(map);
+
+  marker.setMap(map);
+  marker.setVisible(true);
+  marker.directions = directionsDisplay;
+  
+  directionsLayers.push(directionsDisplay);
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    }else{
+    	console.log('routing failed:', status, 'for', start, 'to', end);
+    }
+  });
+
+  showDirectionsButton();
+  hideLoading();
+}
 
 
 window.onload = loadScript;
